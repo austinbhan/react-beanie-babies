73168c7ea4cc3e2d0c2ac0738b81b242224e7d21
austinbhan <austinbhan@outlook.com>
2022-06-22 17:47:37 -0700

    Need to add links

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -1,14 +1,15 @@
-// import { Link } from 'react-router-dom';
 
-export default function BeanieBaby({ title, link, image }) { // Add id when adding Link
+// import { Link } from 'react-router-dom';
+import './App.css';
+
+export default function BeanieBaby({ title, image }) { // Add id when adding Link
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
     // <Link to={`/beanies/${id}`}>
     <div className="beanie-baby">
       <h3>{title}</h3>
-      <p>{link}</p>
-      <img src={image} />
+      <img className="beanie-img" src={image} />
     </div>
     // </Link>  
   );
